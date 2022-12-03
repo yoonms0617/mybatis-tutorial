2f664b98858adab99420f04f90b7b990550dae86
yoonms0617 <yoonminwater@gmail.com>
2022-12-03 20:16:22 +0900

    chore: sql 파일 추가

---
+++ src/main/resources/sql/data/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO MEMBER (name, email) VALUES ('Yoon', 'yoon@test.com');
+INSERT INTO MEMBER (name, email) VALUES ('Song', 'song@test.com');
+INSERT INTO MEMBER (name, email) VALUES ('Kang', 'kang@test.com');
+INSERT INTO MEMBER (name, email) VALUES ('June', 'june@test.com');
